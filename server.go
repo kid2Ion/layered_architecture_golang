1671d395155374fba1f10be4d3cb17b8f61d1306
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2022-05-28 18:03:07 +0900

    routing

--- server.go
+++ server.go
@@ -3,15 +3,21 @@ package main
 import (
 	"net/http"
 
+	rice "github.com/GeertJohan/go.rice"
 	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", hello)
+	files := http.FileServer(rice.MustFindBox("public").HTTPBox())
+
+	e.GET("/", index)
+	// urlから/staticを削除して、残りのファイル名をpublic配下から返却
+	e.GET("/static/*", echo.WrapHandler(http.StripPrefix("/static/", files)))
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-func hello(c echo.Context) error {
+func index(c echo.Context) error {
 	return c.String(http.StatusOK, "hello world")
 }
