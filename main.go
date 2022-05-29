cf7515cbfb0a0e727674855a44eee504b37942b0
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2022-05-29 16:30:36 +0900

    reset

--- main.go
+++ main.go
@@ -3,16 +3,13 @@ package main
 import (
 	"net/http"
 
-	rice "github.com/GeertJohan/go.rice"
 	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
-	files := http.FileServer(rice.MustFindBox("public").HTTPBox())
 
 	e.GET("/", index)
-	e.GET("/static/*", echo.WrapHandler(http.StripPrefix("/static/", files)))
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
